e25c37581b0ad13a502c75e3a73456b8c397efe1
David Thomas <d.thomas1@imperial.ac.uk>
2015-01-23 13:12:35 +0000

    One more time...

--- prepare_submissions.sh
+++ prepare_submissions.sh
@@ -8,7 +8,7 @@ else
 	FILES="$FILES .git";
 fi
 
-WANTED="makefile audio/makefile audio/passthrough.c audio/print_audio.c audio/signal_generator.c audio/fir_filter.c";
+WANTED="makefile audio/makefile audio/passthrough.c audio/print_audio.c audio/signal_generator.c audio/fir_filter.c audio/merge.cpp";
 WANTED="${WANTED} audio/corrupter.sh audio/all_firs_direct.sh audio/all_firs_staged.sh";
 WANTED="${WANTED} audio/mp3_file_src.sh audio/mp3_file_src.sh audio/audio_sink.sh audio/all_firs_staged.sh";
 
